50eec169ab0bbd4227795d3941f9b72b201c8e13
velishanadya96 <velishanadya96@gmail.com>
2026-05-27 16:02:41 +0700

    tikom

--- api/koneksi.php
+++ api/koneksi.php
@@ -3,7 +3,7 @@ $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
 $port = '4000';
 $user = '2fwbX5uBv2zabko.root';
 $pass = 'fJ6laxRpWkcYyNd1';
-$db   = 'db_coffeepay'
+$db   = 'db_coffeepay';
 // TiDB Cloud wajib SSL
 
 $koneksi = mysqli_init();
